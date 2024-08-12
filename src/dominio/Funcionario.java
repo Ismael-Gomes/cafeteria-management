3a8e2f871a3154ac8b49d88e1f549301684d4f59
Ismael Gomes da Silva <ismaelcraft74@gmail.com>
2024-08-12 09:49:57 -0300

    feat :sparkles: Classe funcionário
    Criação da classe funcionário com todos os atributos utilizados na tabela funcionário do banco de dados.

--- src/dominio/Funcionario.java
+++ src/dominio/Funcionario.java
@@ -1,4 +1,60 @@
 package dominio;
 
 public class Funcionario {
+    private String nome, email, senha, cpf, sitacao, numeroCelular;
+    private int quantidadeVendas;
+    double salario;
+
+    public Funcionario(String nome, String email, String senha, String cpf, double salario, String numeroCelular, String sitacao, int quantidadeVendas) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.salario = salario;
+        this.numeroCelular = numeroCelular;
+        this.sitacao = sitacao;
+        this.quantidadeVendas = quantidadeVendas;
+    }
+
+    public Funcionario(String nome, String email, String cpf, String sitacao, int quantidadeVendas, double salario, String numeroCelular) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.sitacao = sitacao;
+        this.salario = salario;
+        this.numeroCelular = numeroCelular;
+        this.quantidadeVendas = quantidadeVendas;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public String getNumeroCelular(){
+        return numeroCelular;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getSitacao() {
+        return sitacao;
+    }
+
+    public int getQuantidadeVendas() {
+        return quantidadeVendas;
+    }
 }
