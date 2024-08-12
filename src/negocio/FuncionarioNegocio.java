f4addcaf525673078ecf2349fe885cb5bdb903bf
Ismael Gomes da Silva <ismaelcraft74@gmail.com>
2024-08-12 09:51:39 -0300

    feat :sparkles: Chamada do banco
    Adicionei as lógicas na classe negocio para fazer o chamado com minha classe DAO.

--- src/negocio/FuncionarioNegocio.java
+++ src/negocio/FuncionarioNegocio.java
@@ -1,4 +1,19 @@
 package negocio;
 
+import dao.FuncionarioDAO;
+import dominio.Funcionario;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class FuncionarioNegocio {
+    private FuncionarioDAO funcionarioDao = new FuncionarioDAO();
+
+    public List<Funcionario> searchAll() throws SQLException {
+        return funcionarioDao.searchAll();
+    }
+
+    public List<Funcionario> searchByCategory(String cpf) throws SQLException{
+        return funcionarioDao.searchByCPF(cpf);
+    }
 }
