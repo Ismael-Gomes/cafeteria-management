ee2004a0cdea7287f8db07d46fd392e59453d605
Ismael Gomes da Silva <ismaelcraft74@gmail.com>
2024-08-24 08:44:05 -0300

    feat :sparkles: Chamadas SQL
    Adicionei as funcionalidades de chamada da tabela cliente no mysql.

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -33,7 +33,7 @@ public class ClienteDAO {
             String cpf = rs.getString("cpf");
             String nome = rs.getString("nome");
             String sexo = rs.getString("sexo");
-            Date data_nascimento = rs.getDate("data_nascimento");
+            String data_nascimento = rs.getString("data_nascimento");
             String nomeConta = rs.getString("nome_conta");
             String email = rs.getString("email");
             String senhaUsu = rs.getString("senha");
@@ -44,10 +44,9 @@ public class ClienteDAO {
             LOGGER.log(Level.SEVERE, "Erro ao buscar todos os clientes", e);
         }
         return clientes;
-
     }
 
-    public String obterNomePorCPF(String cpf) {
+    public String searchNameByCPF(String cpf) {
         String sql = "SELECT nome FROM cliente WHERE cpf = ?";
         try (Connection conexao = conection();
              PreparedStatement ps = conexao.prepareStatement(sql)){
@@ -60,6 +59,59 @@ public class ClienteDAO {
             throw new RuntimeException("Erro ao buscar nome por CPF", e);
         }
         return null;
+    }
+
+    public void insertClien(Cliente cliente) {
+        String sql = "INSERT INTO cliente ?, ?, ?, ?, ?, ?, ?";
+        try(Connection conexao = conection();
+        PreparedStatement ps = conection().prepareStatement(sql)){
+            ps.setString(1, cliente.getCpf());
+            ps.setString(2, cliente.getNome());
+            ps.setString(3, cliente.getNomeConta());
+            ps.setString(4, cliente.getSexo());
+            ps.setString(5, cliente.getData_nascimento());
+            ps.setString(6, cliente.getEmail());
+            ps.setString(7, cliente.getSenha());
+            ps.executeUpdate();
+        }catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Erro ao inserir cliente", e);
+        }
+    }
+
+    public void updateClien(Cliente cliente) {
+        String sql = "UPDATE cliente SET nome = ?, nome_conta = ?, senha = ? where cpf = ?";
+        String verificaNomeContaSql = "SELECT COUNT(*) FROM cliente WHERE nome_conta = ?";
+        try(Connection conexao = conection();
+            PreparedStatement ps1 = conection().prepareStatement(verificaNomeContaSql);
+            PreparedStatement ps = conection().prepareStatement(sql)){
+            ps.setString(1, cliente.getNome());
+            ps1.setString(1, cliente.getNomeConta());
+            ResultSet rs = ps1.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    System.out.println("O nome da conta já existe, e não pode ser alterado.");
+                }else{
+                    ps.setString(2, cliente.getNomeConta());
+                    ps.setString(3, cliente.getSenha());
+                    ps.setString(4, cliente.getCpf());
+                    ps.executeUpdate();
+                }
+            }
+        }catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Erro ao inserir cliente", e);
+        }
+    }
+
+    public void dropClien(String cpf) {
+        String sql = "DELETE FROM cliente WHERE cpf = ?";
+        try(Connection conexao = conection();
+        PreparedStatement ps = conexao.prepareStatement(sql)){
+            ps.setString(1, cpf);
+            ps.executeUpdate();
+        }catch (SQLException e){
+            LOGGER.log(Level.SEVERE, "Erro ao deletar cliente", e);
+        }
     }
 
 }
